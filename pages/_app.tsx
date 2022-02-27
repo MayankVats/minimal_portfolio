70fdac7abd0ad5b53bb4cacc446b5a08d2d4c2d9
MayankVats <vats.2610@gmail.com>
2022-02-27 14:40:41 +0530

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-9QHW6VDTQF`}
       />
 
       <Script id="google-analytics" strategy="lazyOnload">
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', 'G-9QHW6VDTQF', {
               page_path: window.location.pathname,
             });
                 `}
