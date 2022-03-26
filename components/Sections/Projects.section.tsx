bc0b5160de30fe566a3432d87659417bf89831f6
MayankVats <vats.2610@gmail.com>
2022-03-26 07:55:56 +0530

    added projects

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -9,10 +9,8 @@ const Projects: React.FC = () => {
         Featured Projects
       </p>
 
-      <div style={{ color: "white" }}>Coming Soon...</div>
-
-      {/* <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
-        <Link href="" passHref>
+      <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
+        <Link href="https://elbanco.netlify.app/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#D8B4FE] to-[#818CF8] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
@@ -25,20 +23,20 @@ const Projects: React.FC = () => {
           </a>
         </Link>
 
-        <Link href="" passHref>
+        <Link href="https://wolfcoin.netlify.app/" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
           >
             <div className="bg-primary flex h-full w-full flex-col items-center justify-center rounded-lg px-2 text-center font-medium">
-              <p className="text-xl font-semibold">Jalebiswap</p>
-              <p>Decentralized Exchange, a fork of UniswapV2</p>
+              <p className="text-xl font-semibold">WolfCoin</p>
+              <p>An ERC20 Token Dapp</p>
             </div>
           </a>
         </Link>
 
-        <Link href="" passHref>
+        {/* <Link href="" passHref>
           <a
             className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FDE68A] via-[#FCA5A5] to-[#FECACA] p-1 text-white duration-100 hover:scale-105"
             target="_blank"
@@ -49,8 +47,8 @@ const Projects: React.FC = () => {
               <p>A multi sig wallet app.</p>
             </div>
           </a>
-        </Link>
-      </div> */}
+        </Link> */}
+      </div>
     </div>
   );
 };
